d07d0ce53f9f901581547a857f62f3f7c52b6967
Filippo Berto <filippoberto95@gmail.com>
2017-01-09 10:16:04 +0100

    Selezione della regione migliorata via javascript

--- settings/script_select_provincia.php
+++ settings/script_select_provincia.php
@@ -2,5 +2,21 @@
 
 require_once realpath(dirname(__FILE__))."/../lib/php/select_provincia.php";
 
+if(isset($_GET)) {
+
+    if(isset($_GET['regione'])) {
+        $result = SelectProvincia::getProvince($_GET['regione']);
+
+        foreach ($result as $key => $value) {
+            printf(
+                "<option value='%s'>%s</option>\n",
+                $value['nome'],
+                $value['nome']
+            );
+        }
+    }
+
+}
+
 
     ?>
