6f07039da99580c4b48567fe7ed62bd80d7bd75a
oigroig <giorgio.giuffre@studenti.unipd.it>
2017-02-04 16:35:41 +0100

    CSS

--- lib/php/header.php
+++ lib/php/header.php
@@ -1,17 +1,14 @@
 <?php
 
-$nomeSito = 'BandBoard';
-
 # Classe per la generazione dell'header
 class Header {
 
 	# ritorna una stringa con l'header
 	public static function getHeader() {
-		global $nomeSito;
+		$nomeSito = 'BandBoard';
 		return	'<div id="header">' .
 				'<h1 xml:lang="en" lang="en">' . $nomeSito . '</h1>' .
-#					'<img src="/images/logo.png" />'
-				'</div>';
+			'</div>';
 	}
 }
 
