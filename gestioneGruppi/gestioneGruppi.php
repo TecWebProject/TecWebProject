b3b5da1feb76861c854cf82f0114744773bc5f09
oigroig <giorgio.giuffre@studenti.unipd.it>
2017-02-04 09:30:24 +0100

    aggiustata l'indentazione

--- gestioneGruppi/gestioneGruppi.php
+++ gestioneGruppi/gestioneGruppi.php
@@ -25,83 +25,90 @@ $page .= Start::getHead(
 	)
 );
 
-$page .= "<body>";
+$page .= '<body>';
 $page .= Header::getHeader();
 
-if (isset($_SESSION['username'])) {	//UTENTE LOGGATO
-	$logout="<div class=\"logout\">
-                <form action=\"../lib/php/logout.php\" method=\"post\">
-	                <p><input type=\"submit\" id=\"logout\" value=\"Logout\" /></p>
-                </form>
-            </div>";
+# creazione del menù ed eventuale pulsante logout per utente registrato
+if (isset($_SESSION['username'])) {
+	$logout = '<div class="logout">' .
+		'<form action="../lib/php/logout.php" method="post">' .
+			'<p><input type="submit" id="logout" value="Logout" /></p>' .
+		'</form>' .
+	'</div>';
 	$page .= $logout;
-	$page .= "<div class=\"nav\">".Menu::getMenu(array(
-		'<a href="../index.php" xml:lang="en" lang="en">Home</a>',
-		"<a href='../settings/index.php'>Modifica Profilo</a>",
-		"<a href='../cercaUtenti/index.php'>Cerca Utenti</a>",
-		"<a href='../cercaGruppi/index.php'>Cerca Gruppi</a>",
-		"I Miei Gruppi")
-		)."</div>";	//CREAZIONE DEL MENU PER UTENTE LOGGATO
+	$page .= '<div class="nav">' . Menu::getMenu(
+		array(
+			'<a href="../index.php" xml:lang="en" lang="en">Home</a>',
+			'<a href="../settings/index.php">Modifica Profilo</a>',
+			'<a href="../cercaUtenti/index.php">Cerca Utenti</a>',
+			'<a href="../cercaGruppi/index.php">Cerca Gruppi</a>',
+			'I Miei Gruppi'
+		)
+	) . '</div>';
 } else {
 	session_unset();
 	session_destroy();
-	$page .= "<div class=\"nav\">".Menu::getMenu(array('<a href="../index.php" xml:lang="en" lang="en">Home</a>', "<a href='../cercaUtenti/index.php'>Cerca Utenti</a>", "<a href='../cercaGruppi/index.php'>Cerca Gruppi</a>"))."</div>";	//CREAZIONE DEL MENU PER UTENTE NON LOGGATO
+	$page .= '<div class="nav">' . Menu::getMenu(
+		array(
+			'<a href="../index.php" xml:lang="en" lang="en">Home</a>',
+			'<a href="../cercaUtenti/index.php">Cerca Utenti</a>',
+			'<a href="../cercaGruppi/index.php">Cerca Gruppi</a>'
+		)
+	) . '</div>';
 }
-$page .= file_get_contents(realpath(dirname(__FILE__))."/gestioneGruppi.txt");
+
+$page .= file_get_contents('gestioneGruppi.txt');
 
 //connessione al db
 
-$connessione=dbConnectionData::getMysqli();	//CONNESSIONE AL DATABASE
+$conn = dbConnectionData::getMysqli();	//CONNESSIONE AL DATABASE
 try {
-	if ($connessione->connect_errno) {
-		throw new Exception("Connessione fallita: ".$connessione->connect_error.".");
+	if ($conn->connect_errno) {
+		throw new Exception("Connessione fallita: ".$conn->connect_error.".");
 	} else {
-		$query="SELECT G.nome, G.immagine, G.idGruppo FROM Conoscenze C JOIN Formazioni F ON C.idConoscenza=F.ruolo JOIN Gruppi G ON F.gruppo=G.idGruppo WHERE C.utente=\"".$_SESSION['username']."\";";
+		$query = "SELECT G.nome, G.immagine, G.idGruppo
+		FROM Conoscenze C JOIN Formazioni F ON C.idConoscenza = F.ruolo
+		JOIN Gruppi G ON F.gruppo=G.idGruppo
+		WHERE C.utente='" . $_SESSION['username'] . "'";
 		$gruppi="";
-			if (!$result=$connessione->query($query)) {
-					echo "Query non valida: ".$connessione->error.".";
-			} else {
-					if ($result->num_rows>0) { //se appartiene a 1 o + gruppi stampa immagine e gruppo
-						$gruppi="<ul>";
-							while ($row=$result->fetch_array(MYSQLI_ASSOC)) {
-								if ($row['immagine']==NULL) {
-										$row['immagine']="defaultBand.png";
-										$gruppi=$gruppi."<li class=\"elementResult\"><a href=\"profiloGruppo/profiloGruppo.php?idGruppo=".$row['idGruppo']."&page=index\"><img class=\"listImage\" src=\"images/site/".$row['immagine']."\" alt=\"Immagine di ".$row['nome']."\" /> ".$row['nome'];
-										//link alla pagina di modifica della band 
-										$gruppi=$gruppi."<li class=\"updates\"><a href=\"modificaGruppo/modificaGruppo.php?idGruppo=".$row['idGruppo']."&page=index\">";
-										$page=str_replace("<modifica />", $modifica, $page);
-										//form per lasciare una band
-										$lascia="<div class=\"lascia\">
-											<form action=\"../lib/php/to do.php\" method=\"post\">
-												<p><input type=\"submit\" id=\"lascia\" value=\"Lascia\" /></p>
-											</form>
-										</div>";
-										$page=str_replace("<lascia />", $lascia, $page);
-										
-								} else {
-										$gruppi=$gruppi."<li class=\"elementResult\"><a href=\"profiloGruppo/profiloGruppo.php?idGruppo=".$row['idGruppo']."&page=index\"><img class=\"listImage\" src=\"images/site/".$row['immagine']."\" alt=\"Immagine di ".$row['nome']."\" /> ".$row['nome'];
-								}						
-								$gruppi=$gruppi."</a></li>";
-							}
-							$result->free();
-							$gruppi=$gruppi."</ul>";
-					}
-					else { //altrimenti stampa
-						$gruppi=$gruppi."<p>Al momento non sei membro di nessuna band.</p>";}
+		if (!$result=$conn->query($query)) {
+				echo 'Query non valida: ' . $conn->error . '.';
+		} else {
+			if ($result->num_rows > 0) { # se appartiene a 1 o più gruppi, stampa immagine e gruppo
+				$gruppi = '<ul>';
+				while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+					if ($row['immagine'] == NULL) {
+						$row['immagine'] = '../images/site/defaultBand.png';
+						$gruppi .= '<li class="elementResult"><a href="../profiloGruppo/profiloGruppo.php?idGruppo=' . $row['idGruppo'] . '"><img class="listImage" src="../images/site/' . $row['immagine'] . '" alt="Immagine di "' . $row['nome'] . "' /> " . $row['nome'];
+						# link alla pagina di modifica della band 
+						$gruppi .= '<li class="updates"><a href="../modificaGruppo/modificaGruppo.php?idGruppo=' . $row['idGruppo'] . '">';
+						$page = str_replace('<modifica />', $modifica, $page);
+						# form per lasciare una band
+						$lascia = '<div class="lascia">' .
+							'<form action="../lib/php/to do.php" method="post">' .
+								'<p><input type="submit" id="lascia" value="Lascia" /></p>' .
+							'</form>' .
+						'</div>';
+						$page = str_replace('<lascia />', $lascia, $page);
+					} else {
+						$gruppi .= '<li class="elementResult"><a href="profiloGruppo/profiloGruppo.php?idGruppo=' . $row['idGruppo'] . '&page=index"><img class="listImage" src="images/site/' . $row['immagine'] . '" alt="Immagine di ' . $row['nome'].'" /> ' . $row['nome'];
+					}						
+					$gruppi .= "</a></li>";
+				}
+				$result->free();
+				$gruppi .= '</ul>';
 			}
-			$page=str_replace("<nomeGruppi />", $gruppi, $page);
-			
-			//per vedere che str_replace funziona con le form
-				$lascia="<div class=\"lascia\">
-	                <form action=\"../lib/php/to do.php\" method=\"post\">
-    	                <p><input type=\"submit\" id=\"lascia\" value=\"Lascia\" /></p>
-	                </form>
-                </div>";
-				$page=str_replace("<lascia />", $lascia, $page);
+			else { # altrimenti stampa
+				$gruppi .= '<p>Al momento non sei membro di nessun gruppo.</p>';
+			}
 		}
-		}catch (Exception $e){
-			echo "Errore: dati non recuperati (".$e->getMessage().").";
+		$page = str_replace('<nomeGruppi />', $gruppi, $page);
 	}
-	$page=str_replace("<footer />", Footer::getFooter(), $page);
-	echo $page;
+} catch (Exception $e){
+	echo 'Errore: dati non recuperati (' . $e->getMessage() . ').';
+}
+
+$page = str_replace('<footer />', Footer::getFooter(), $page);
+echo $page;
+
 ?>
